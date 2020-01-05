13c1bec557c5a711c42ed1f27e932c7c58fe766a
zombiedz23 <59516465+zombiedz23@users.noreply.github.com>
2020-01-05 22:14:24 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "#";
+var prefix = "E";
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
@@ -23,7 +23,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`S2ud`,"http://twitch.tv/S-F")
+client.user.setGame(`يا انتا يا حلو ..`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
